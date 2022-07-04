66a60b778dacc82ec51db75722c467f2a35135e9
Russell Hickey <russell.hickey@mongodb.com>
2022-07-04 11:44:51 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami     = data.aws_ami.amazon-linux-2.id
+  instance_type     = "t2.micro"
+
+  tags = {
+    Name = "RussellsExampleServer"
+  }
+}
+
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+
+  owners = ["amazon"]
+}
